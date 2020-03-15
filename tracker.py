bacd38a04a35fa1a479f81bb13fc3eccd1d6fc76
FadelNasereddin <50805022+FadelNasereddin@users.noreply.github.com>
2020-03-14 22:46:42 -0400

    created message body template

--- tracker.py
+++ tracker.py
@@ -10,6 +10,8 @@ page= requests.get(URL,headers=headers)
 
 soup=bs(page.content, "html.parser")
 
+sender= 'amazonPriceTracker1010@gmail.com'
+password='avhbkcuorkvhhyzb'
 def price_checker():
 title= soup.find(id='productTitle').get_text()
 price= soup.find(id='priceblock_ourprice').get_text()
@@ -19,8 +21,21 @@ if clean_price < 128:
     send_mail()
 
 
-def send_mail():
+def send_mail(sender,password,receiver='fadelnasereddin6@gmail.com'):
     server= smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(email,password)
+
+    subject= "Yo man I found a Great Deal for you!"
+    body= "This is is the link for the discounted product: \n 'https://www.amazon.ca/Keurig-K-Select-Coffee-Maker-Matte/dp/B07B8SQXLK/ref=sr_1_1_sspa?crid=3M68BKCL5AGLC&keywords=keurig+coffee+maker&qid=1584239335&sprefix=keurgi%2Caps%2C272&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExT0tKODFHSEFCUjBZJmVuY3J5cHRlZElkPUExMDQ1MjQ0M09PQ0RSWDE0QjBSSCZlbmNyeXB0ZWRBZElkPUEwMTE1MTI1M002NjJPRkpJWFRSTiZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU='"
+    message= f"Subject: {subject} \n \n {body}"
+
+    server.sendmail(
+        sender,
+        receiver,
+        message
+    )
+    print('MESSAGE SENT SUCCESSFULLY')
     
-
-
